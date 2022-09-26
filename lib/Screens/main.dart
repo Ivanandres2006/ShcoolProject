740fbbecf07d3d99dc7011d6f2af93e5c6ddc423
Ivanandres2006 <71884124+Ivanandres2006@users.noreply.github.com>
2022-09-26 06:50:31 -0400

    Add files via upload

---
+++ lib/Screens/main.dart
@@ -0,0 +1,27 @@
+// ignore: depend_on_referenced_packages
+import 'package:colegio_app/Home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'School App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: home_screen(),
+    );
+  }
+}
---
+++ lib/models/Todos.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TodoItem extends StatefulWidget {
+  const TodoItem({super.key});
+
+  @override
+  State<TodoItem> createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
